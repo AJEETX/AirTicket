9832110f243161795dae8d6de6a981e272ad806b
Ajeet <ajeet.tiwari@helloworld.com.au>
2018-02-24 18:23:18 +1100

    first commit

---
+++ XMLtoHTML/Display/ConvertXMLtoHTML.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Xml.Xsl;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Display
+{
+    class ConvertXMLtoHTML
+    {
+        static IEnumerable<string> files; static int count = 0;
+
+        //ConcurrentQueue thread-safe first in-first out (FIFO) collection
+       static  ConcurrentQueue<Tuple<string>> cq = new ConcurrentQueue<Tuple<string>>();
+       
+       #region  Get the list of all xml files from the folder i.e. \Data\Computers
+
+       internal static int ReadAllFileswithPattern(string filePath, string searchPattern)
+       {
+           try
+           {
+               //get all the file with searchPattern as *.xml
+               files = Directory.EnumerateFiles(filePath, searchPattern);
+            
+               //count the total read files
+              count= IterateFiles(files, filePath);
+           }
+           catch (IOException io)
+           {
+               MessageBox.Show("Some IO exception during Read:" + io.Message);
+           }
+           catch (NullReferenceException e)
+           {
+               MessageBox.Show("Some Null reference exception during Read:" + e.Message);
+           }
+           return count;
+       }
+
+       #endregion
+
+       #region convert xml file to html with xsl stylesheet
+
+       internal static int TransformXMLToHTML(string xslFilePath, string xslFile,string xmlPath)
+       {
+            Tuple<string> tpl; 
+           
+           //get full path of xslt file from source locatoin
+           string fullPath = Path.Combine(xslFilePath, xslFile);
+
+           //Ignore DTD processing and set no validation 
+           XmlReaderSettings xmlReadersettings = new XmlReaderSettings()
+           {
+               DtdProcessing = DtdProcessing.Ignore,ValidationType=ValidationType.None
+           };
+
+           //Declare and create a new XSLT processor object that implements the XSLT
+           XslCompiledTransform xslTransform = new XslCompiledTransform();
+           
+           try
+           {
+               //Load the XSL
+               xslTransform.Load(XmlReader.Create(fullPath, xmlReadersettings));
+               
+               //Loop till count equals 0 while deach dequeue decreases count by 1
+               while (!cq.IsEmpty)
+               {
+                   // dequeue the value from tuple
+                   cq.TryDequeue(out tpl);
+ 
+                   //Transform the xml to an html output and with same name but with html extension instead of xml
+                   xslTransform.Transform(tpl.Item1, xmlPath + @"\" + Path.GetFileNameWithoutExtension(tpl.Item1) + ".html");
+               }
+           }
+           catch (IOException io)
+           {
+               MessageBox.Show("Some IO exception during transform:" + io.Message);            
+           }
+           catch (XmlException xmlEx)
+           {
+               MessageBox.Show("Some xml exception during transform:" + xmlEx.Message);
+           }
+              
+           return cq.Count();
+       }
+
+       #endregion
+
+       #region Read all files and its contents & store in queue
+
+       internal static int IterateFiles(IEnumerable<string> files, string directory)
+       {
+           Tuple<string> tpl = null; 
+           
+           //loop for all files with xml extension and enqueue them
+           foreach (var file in files)
+           {
+               //for verification of dirctory and file  
+               Console.WriteLine("{0}", Path.Combine(directory, file));
+               
+               try
+               {
+                   //store file on tuple
+                   tpl = new Tuple<string>(file);
+
+                   //Enqueue the tuple
+                   cq.Enqueue(tpl);
+               }
+               catch (IOException io)
+               {
+                   MessageBox.Show("Some IO exception during Iterate:" + io.Message);
+               }
+               catch (XmlException xmlEx)
+               {
+                   MessageBox.Show("Some xml exception during Iterate:" + xmlEx.Message);
+               }
+           }
+           return cq.Count;
+       }
+       #endregion
+
+       #region Copy the input files i.e css file to destination folder
+
+       internal static void CopyInputFile(string sourcefilePath,string destinationFilePath, string searchPattern)
+       {
+           // search for css file from the source path 
+           var filename = Directory.GetFiles(sourcefilePath, searchPattern).SingleOrDefault().ToString();
+           
+           string sourceFileName = filename;
+           
+           //set the destinatoin path file name
+           string destinationFileName = destinationFilePath + @"\" +Path.GetFileName(filename);
+           
+           //copy css from source path to the destination path i.e. Output folder
+           File.Copy(sourceFileName, destinationFileName, true);
+       }
+       #endregion
+    }
+}
---
+++ XMLtoHTML/Display/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+using System.Configuration;
+
+namespace Display
+{
+    public partial class Form1 : Form
+    {
+        #region Intialize variables
+        Thread readThread, transformThread;
+
+         string searchXMLpattern = ConfigurationManager.AppSettings["xmlFilePattern"].ToString();
+         string searchHTMLpattern = ConfigurationManager.AppSettings["htmlFilePattern"].ToString();
+
+         int countNumberReadFiles = 0;
+        string searchCSSpattern = ConfigurationManager.AppSettings["cssFilePattern"].ToString();
+        //assign the path of xml files to read from configuration file
+        string xmlFilePath = Environment.CurrentDirectory + ConfigurationManager.AppSettings["xmlFilePath"].ToString();
+
+        //assign the Computer.xslt file path from configuration file
+        string xslFilePath = Environment.CurrentDirectory + ConfigurationManager.AppSettings["xslFilePath"].ToString();
+
+        // set the output path of to be transformed html files with configuration file
+        string htmlOutputFilePath = Environment.CurrentDirectory + ConfigurationManager.AppSettings["htmlFilePath"].ToString();
+        
+        // set the xslt file name through configuration file
+        string xslFile = ConfigurationManager.AppSettings["xslFileName"].ToString();
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        // Read the xml files 
+        private void ReadFiles()
+        {
+            //Read all the files with search pattern in the folder i this case its for searching xml files
+            ConvertXMLtoHTML.ReadAllFileswithPattern(xmlFilePath, searchXMLpattern);
+        }
+
+        private void MakeHTML()
+        {
+            int countFilesRemainingtoTransform = ConvertXMLtoHTML.TransformXMLToHTML(xslFilePath, xslFile, htmlOutputFilePath);
+            
+            //If all the files on the queue have been converted i.e. no files remaining then show the sucess message
+            if (countFilesRemainingtoTransform == 0)
+            {
+                //do not show picturebox as the transform is complete
+                EnablePictureBoxVisibility(pictureBox1, false);
+
+                //Open a window stating that the transform is complete
+                MessageBox.Show("XML files converted to html at Date Time:"+ DateTime.Now.ToLocalTime(), "XML to HTML conversion", MessageBoxButtons.OK);
+
+                //open the folder where html files are created
+                Process.Start(htmlOutputFilePath);
+                
+                // close the application
+                Application.Exit();
+            }
+        }
+        //Delete the Output folder if exists and put style.css file within
+        private void DeleteOutputFolderFiles()
+        {
+            //check if output directory exits
+            if (!Directory.Exists(htmlOutputFilePath))
+            {
+                //create a new Output folder
+                Directory.CreateDirectory(htmlOutputFilePath);
+            }
+                ConvertXMLtoHTML.CopyInputFile(xslFilePath, htmlOutputFilePath, searchCSSpattern);
+        }
+
+        //Make picturebox visible during transform process and invisible on completion of transform regardless of the thread context you are in
+        private void EnablePictureBoxVisibility(PictureBox pb, bool enable)
+        {
+            //check if the caller method should call invoke method when making call to the control as caller is on different thread
+            if (!pb.InvokeRequired)
+            {
+                //set the visibility of picturebox
+                pb.Visible = enable;
+            }
+            else
+            {
+
+                pb.Invoke(new EnablePictureBoxVisibilityHandler(EnablePictureBoxVisibility), pb, enable);
+            }
+        }
+        //create a delegate and invoke the delegate using control.Invoke
+        delegate void EnablePictureBoxVisibilityHandler(PictureBox btn, bool enable);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //show picturebox
+            EnablePictureBoxVisibility(pictureBox1, true);
+
+            // Call the method to Delete the output folder and files within 
+            DeleteOutputFolderFiles();
+                
+            //Disable the button
+            button1.Enabled = false;
+            
+            //create the thread to read the xml files
+            readThread = new Thread(new ThreadStart(ReadFiles));
+            
+            //start the thread for reading the xml files
+            readThread.Start();
+
+            //create a new thread to transform the read files 
+            transformThread = new Thread(new ThreadStart(MakeHTML));
+
+            //start the thread to transform XML to HTML files
+            transformThread.Start();
+        }
+    }
+}
