0d3d67aed2dcdd8296c9e60cddb4674a8daa1b50
Ajeet <ajeet.tiwari@helloworld.com.au>
2018-02-24 18:49:09 +1100

    refactor

--- XMLtoHTML/Display/Form1.cs
+++ XMLtoHTML/Display/Form1.cs
@@ -21,18 +21,10 @@ namespace Display
 
          string searchXMLpattern = ConfigurationManager.AppSettings["xmlFilePattern"].ToString();
          string searchHTMLpattern = ConfigurationManager.AppSettings["htmlFilePattern"].ToString();
-
         string searchCSSpattern = ConfigurationManager.AppSettings["cssFilePattern"].ToString();
-        //assign the path of xml files to read from configuration file
         string xmlFilePath = Environment.CurrentDirectory + ConfigurationManager.AppSettings["xmlFilePath"].ToString();
-
-        //assign the Computer.xslt file path from configuration file
         string xslFilePath = Environment.CurrentDirectory + ConfigurationManager.AppSettings["xslFilePath"].ToString();
-
-        // set the output path of to be transformed html files with configuration file
         string htmlOutputFilePath = Environment.CurrentDirectory + ConfigurationManager.AppSettings["htmlFilePath"].ToString();
-        
-        // set the xslt file name through configuration file
         string xslFile = ConfigurationManager.AppSettings["xslFileName"].ToString();
 
         #endregion
@@ -40,13 +32,10 @@ namespace Display
         public Form1()
         {
             InitializeComponent();
-
         }
 
-        // Read the xml files 
         private void ReadFiles()
         {
-            //Read all the files with search pattern in the folder i this case its for searching xml files
             ConvertXMLtoHTML.ReadAllFileswithPattern(xmlFilePath, searchXMLpattern);
         }
 
@@ -70,7 +59,6 @@ namespace Display
                 Application.Exit();
             }
         }
-        //Delete the Output folder if exists and put style.css file within
         private void DeleteOutputFolderFiles()
         {
             //check if output directory exits
@@ -81,8 +69,6 @@ namespace Display
             }
                 ConvertXMLtoHTML.CopyInputFile(xslFilePath, htmlOutputFilePath, searchCSSpattern);
         }
-
-        //Make picturebox visible during transform process and invisible on completion of transform regardless of the thread context you are in
         private void EnablePictureBoxVisibility(PictureBox pb, bool enable)
         {
             //check if the caller method should call invoke method when making call to the control as caller is on different thread
@@ -97,29 +83,15 @@ namespace Display
                 pb.Invoke(new EnablePictureBoxVisibilityHandler(EnablePictureBoxVisibility), pb, enable);
             }
         }
-        //create a delegate and invoke the delegate using control.Invoke
         delegate void EnablePictureBoxVisibilityHandler(PictureBox btn, bool enable);
         private void button1_Click(object sender, EventArgs e)
         {
-            //show picturebox
             EnablePictureBoxVisibility(pictureBox1, true);
-
-            // Call the method to Delete the output folder and files within 
             DeleteOutputFolderFiles();
-                
-            //Disable the button
             button1.Enabled = false;
-            
-            //create the thread to read the xml files
             readThread = new Thread(new ThreadStart(ReadFiles));
-            
-            //start the thread for reading the xml files
             readThread.Start();
-
-            //create a new thread to transform the read files 
             transformThread = new Thread(new ThreadStart(MakeHTML));
-
-            //start the thread to transform XML to HTML files
             transformThread.Start();
         }
     }
